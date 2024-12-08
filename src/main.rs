5460375c35a036f459e2932cd3260503682517ab
Swift <swiftshineexists@gmail.com>
2024-12-07 19:12:55 -0600

    disable debug window on windows

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 mod quilt;
 use quilt::QuiltApp;
+
 
 fn main() -> Result<(), eframe::Error> {
     QuiltApp::run()
